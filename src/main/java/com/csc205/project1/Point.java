9bc0fba4bad64ade07d8ff64ce0cb982ee57f793
csaim11 <moz2130044@maricopa.edu>
2025-02-05 21:36:09 -0700

    Project 1 Point & PointTest

--- src/main/java/com/csc205/project1/Point.java
+++ src/main/java/com/csc205/project1/Point.java
@@ -1,4 +1,107 @@
 package com.csc205.project1;
 
+
+/*
+
+    In Java,
+
+    Design & implement a class called Point that represents a location in the Cartesian plane.
+    Include (at a minimum) the following methods:
+
+    A constructor that takes x and y coordinates as double arguments
+    setX(double x), setY(double y) - standard setters
+    getX(), getY() - standard getters
+    setPoint(double x, double y) - set the coordinates of the point.
+    shiftX(double n), shiftY(double n) - shift a point by a given amount along one of the axes.
+    distance(Point p2) - finds the distance to point p2.
+    rotate(double angle) - rotates the point by a specified (radian) angle around the origin.
+    Include a toString method as well.
+
+ */
+
+
 public class Point {
+
+    private double x;
+    private double y;
+
+    // Constructor that takes x and y coordinates as double arguments
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Standard setters
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // Standard getters
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    // Set the coordinates of the point
+    public void setPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Shift a point by a given amount along the x-axis
+    public void shiftX(double n) {
+        this.x += n;
+    }
+
+    // Shift a point by a given amount along the y-axis
+    public void shiftY(double n) {
+        this.y += n;
+    }
+
+    // Finds the distance to point p2
+    public double distance(Point p2) {
+        return Math.sqrt(Math.pow(this.x - p2.x, 2) + Math.pow(this.y - p2.y, 2));
+    }
+
+    // Rotates the point by a specified (radian) angle around the origin
+    public void rotate(double angle) {
+        double newX = this.x * Math.cos(angle) - this.y * Math.sin(angle);
+        double newY = this.x * Math.sin(angle) + this.y * Math.cos(angle);
+        this.x = newX;
+        this.y = newY;
+    }
+
+    // Returns a string representation of the point
+    @Override
+    public String toString() {
+        return "Point(" + "x=" + x + ", y=" + y + ')';
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        Point p1 = new Point(3, 4);
+        Point p2 = new Point(6, 8);
+
+        System.out.println("Initial coordinates of p1: " + p1);
+        System.out.println("Initial coordinates of p2: " + p2);
+
+        p1.shiftX(2);
+        p1.shiftY(3);
+
+        System.out.println("Coordinates of p1 after shifting: " + p1);
+
+        double distance = p1.distance(p2);
+        System.out.println("Distance between p1 and p2: " + distance);
+
+        p1.rotate(Math.PI / 4); // Rotate p1 by 45 degrees
+        System.out.println("Coordinates of p1 after rotation: " + p1);
+    }
+
 }
--- src/test/java/com/csc205/project1/PointTest.java
+++ src/test/java/com/csc205/project1/PointTest.java
@@ -1,91 +1,83 @@
 package com.csc205.project1;
 
+
+/*
+
+    In Java,
+
+    generate unit tests for Class Point, including edge cases
+
+ */
+
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class PointTest {
+public class PointTest {
 
     @Test
-    void setPoint() {
-
-        // Setup
-        double expectedX = 5;
-        double expectedY = 5;
-        Point point = new Point(expectedX, expectedY);
-
-        // Exercise
-        double x = point.getX();
-        double y = point.getY();
-
-        // Assert
-        assertEquals(expectedX, x);
-        assertEquals(expectedY, y);
+    public void testConstructorAndGetters() {
+        Point p = new Point(3.0, 4.0);
+        assertEquals(3.0, p.getX());
+        assertEquals(4.0, p.getY());
     }
 
     @Test
-    void shiftX() {
-
-        // Setup
-        double expectedX = 6;
-        Point point = new Point(5, 5);
-
-        // Exercise
-        point.shiftX(1);
-        double x = point.getX();
-
-        // Assert
-        assertEquals(expectedX, x);
-
+    public void testSetters() {
+        Point p = new Point(0.0, 0.0);
+        p.setX(5.0);
+        p.setY(6.0);
+        assertEquals(5.0, p.getX());
+        assertEquals(6.0, p.getY());
     }
 
     @Test
-    void shiftY() {
-
-        // Setup
-        double expectedY = 6;
-        Point point = new Point(5, 5);
-
-        // Exercise
-        point.shiftY(1);
-        double y = point.getY();
-
-        // Assert
-        assertEquals(expectedY, y);
+    public void testSetPoint() {
+        Point p = new Point(0.0, 0.0);
+        p.setPoint(7.0, 8.0);
+        assertEquals(7.0, p.getX());
+        assertEquals(8.0, p.getY());
     }
 
     @Test
-    void distance() {
-
-        // Setup
-        double expectedDistance = 5;
-        Point point1 = new Point(0, 0);
-        Point point2 = new Point(3,4);
-
-        // Exercise
-
-        double distance = point1.distance(point2);
-
-        // Assert
-        assertEquals(expectedDistance, distance);
+    public void testShiftX() {
+        Point p = new Point(1.0, 1.0);
+        p.shiftX(3.0);
+        assertEquals(4.0, p.getX());
     }
 
     @Test
-    void rotate() {
+    public void testShiftY() {
+        Point p = new Point(1.0, 1.0);
+        p.shiftY(3.0);
+        assertEquals(4.0, p.getY());
+    }
 
-        // Setup
-        double expectedX = -5;
-        double expectedY = 5;
-        Point point = new Point(5, 5);
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(0.0, 0.0);
+        Point p2 = new Point(3.0, 4.0);
+        assertEquals(5.0, p1.distance(p2), 0.0001);
+    }
 
-        // Exercise
-        point.rotate(Math.PI / 2.0);
-        double x = point.getX();
-        double y = point.getY();
+    @Test
+    public void testRotate() {
+        Point p = new Point(1.0, 0.0);
+        p.rotate(Math.PI / 2); // Rotate by 90 degrees
+        assertEquals(0.0, p.getX(), 0.0001);
+        assertEquals(1.0, p.getY(), 0.0001);
+    }
 
-        // Assert
-        assertEquals(expectedX, x);
-        assertEquals(expectedY, y);
+    @Test
+    public void testRotate360Degrees() {
+        Point p = new Point(1.0, 0.0);
+        p.rotate(2 * Math.PI); // Rotate by 360 degrees
+        assertEquals(1.0, p.getX(), 0.0001);
+        assertEquals(0.0, p.getY(), 0.0001);
+    }
+
+    @Test
+    public void testToString() {
+        Point p = new Point(9.0, 10.0);
+        assertEquals("Point(x=9.0, y=10.0)", p.toString());
     }
 }
